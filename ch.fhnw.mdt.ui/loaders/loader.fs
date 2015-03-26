c91a9147ed8dd64b7b0b42b0e3b974872bb34f1a
root <root@ubuntu>
2015-03-27 00:20:35 +0100

    - update preferences

---
+++ ch.fhnw.mdt.ui/loaders/loader.fs
@@ -0,0 +1,46 @@
+\
+\ Last_change: KS 19.03.2013 09:21:22
+\
+\ MicroCore load screen for the core test program that is transferred
+\ into the program memory via the debug umbilical
+\
+Only Forth also definitions hex
+
+[IFDEF] empty unpatch close-port empty [ENDIF] Marker empty
+
+include gforth-config.fs        \ Some System word (re)definitions for a more sympathetic environment
+include vhdl.fs                 \ simple VHDL interpreter for constants
+include ../uCore/constants.vhd
+include microcore.fs            \ the cross-compiler
+include images.fs               \ object code output files
+include disasm.fs               \ the disassembler
+include umbilical.fs            \ defines the actual serial port used on this computer
+include constants.fs            \ MicroCore Register addresses and bits
+
+new Target                      \ reset target compiler and go into target compilation mode
+
+8 trap-addr code-origin
+          0 data-origin  Initialised
+
+include forth.fs
+include debugger.fs
+Uninitialised
+include multitask.fs
+
+include $INPUT_FILE
+
+: boot  ( -- )
+   0 #top erase  CALL Initialisation
+   init   debugService
+;
+
+#reset USR: rst    ( -- ) ['] boot             nop BRANCH ;USR  \ compile branch to TEST at reset vector location
+#isr   USR: isr    ( -- ) ints@ drop                 IRET ;USR
+#esr   USR: esr    ( -- ) ['] exceptionService nop BRANCH ;USR  \ retry instruction
+#osr   USR: osr    ( -- )                            EXIT ;USR  \ ?OVFL Vektor
+#break USR: break  ( -- ) ['] debugger         nop BRANCH ;USR  \ Debugger
+#data! USR: data!  ( dp-1 n -- dp )  swap 1+ st      EXIT ;USR  \ Data memory initialisation
+
+end
+
+\ CRC-file load.crc  cr .( load.fs written to load.crc )
--- ch.fhnw.mdt.ui/src/ch/fhnw/mdt/ui/perferences/MCorePreferencePage.java
+++ ch.fhnw.mdt.ui/src/ch/fhnw/mdt/ui/perferences/MCorePreferencePage.java
@@ -1,36 +1,126 @@
 package ch.fhnw.mdt.ui.perferences;
 
-import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.preference.PreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.jface.preference.IntegerFieldEditor;
-import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.wb.swt.SWTResourceManager;
+import org.osgi.framework.Bundle;
 
-public class MCorePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+import ch.fhnw.mdt.launch.ui.SelectUSBDeviceDialog;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.GridData;
+
+public class MCorePreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
+	private Text usbDeviceText;
+	private Text gforthLoaderText;
 
 	/**
 	 * Create the preference page.
 	 */
 	public MCorePreferencePage() {
-		super(FLAT);
-		setTitle("MCore Preferences");
 	}
 
 	/**
 	 * Create contents of the preference page.
+	 * 
+	 * @param parent
 	 */
 	@Override
-	protected void createFieldEditors() {
-		StringFieldEditor stringFieldEditor = new StringFieldEditor("id", "Device Port", -1, StringFieldEditor.VALIDATE_ON_KEY_STROKE, getFieldEditorParent());
-		stringFieldEditor.setStringValue("/dev/ttyUSB0");
-		addField(stringFieldEditor);
+	public Control createContents(final Composite parent) {
+		final Composite container = new Composite(parent, SWT.NULL);
+		container.setLayout(new GridLayout(3, false));
+
+		final Label lblUsbDevice = new Label(container, SWT.NONE);
+		lblUsbDevice.setText("USB Device");
+
+		usbDeviceText = new Text(container, SWT.BORDER);
+		usbDeviceText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		final Button selectUSBDeviceButton = new Button(container, SWT.NONE);
+		selectUSBDeviceButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(final SelectionEvent e) {
+				final SelectUSBDeviceDialog selectUsbDeviceDialog = new SelectUSBDeviceDialog(getShell());
+				final int response = selectUsbDeviceDialog.open();
+				if (response == SelectUSBDeviceDialog.OK) {
+					final String device = selectUsbDeviceDialog.getSelectedDevice();
+					usbDeviceText.setText(device);
+				}
+			}
+		});
+		selectUSBDeviceButton.setText("Search Device...");
+				
+						final Label label = new Label(container, SWT.SEPARATOR | SWT.HORIZONTAL);
+						label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 3, 1));
+		
+				final Label lblGforthLoader = new Label(container, SWT.NONE);
+				lblGforthLoader.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+				lblGforthLoader.setText("GForth Loader");
+		new Label(container, SWT.NONE);
+		
+		final Label lblNewLabel = new Label(container, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		lblNewLabel.setForeground(SWTResourceManager.getColor(SWT.COLOR_DARK_GRAY));
+		lblNewLabel.setText("The $INPUT_FILE will be replaced by the actual file name.");
+		new Label(container, SWT.NONE);
+
+		gforthLoaderText = new Text(container, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI);
+		GridData gd_gforthLoaderText = new GridData(SWT.FILL, SWT.FILL, false, true, 3, 1);
+		gd_gforthLoaderText.heightHint = 426;
+		gforthLoaderText.setLayoutData(gd_gforthLoaderText);
+
+		gforthLoaderText.setText(getDefaultLoader());
+		
+		return container;
+	}
+	
+	private String getDefaultLoader() {
+		final Bundle bundle = Platform.getBundle("ch.fhnw.mdt.ui");
+		final URL fileURL = bundle.getEntry("loaders/loader.fs");
+		
+		String loader = "";
+		
+		try {
+			final File file = new File(FileLocator.resolve(fileURL).toURI());
+			
+			try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					loader += line;
+					loader += System.lineSeparator();
+				}
+			}
+			
+		} catch (URISyntaxException | IOException e) {
+			e.printStackTrace();
+		}
+		return loader;
 	}
 
 	/**
 	 * Initialize the preference page.
 	 */
-	public void init(IWorkbench workbench) {
+	public void init(final IWorkbench workbench) {
 		// Initialize the preference page
 	}
-
 }
